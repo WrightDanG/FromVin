78cdbfbf92ffa1d52fd3c5425b245552ceb168cd
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-04-10 17:56:54 +0000

    Begin to add allauth for backend admin system.

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',  # Only needed if we want FB/Google logins.
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',  # Only needed if we want FB/Google logins.
 ]
 
 MIDDLEWARE = [
@@ -66,6 +70,17 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Taken from the allauth docs
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'fromvin.wsgi.application'
 
