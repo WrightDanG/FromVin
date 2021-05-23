9622170c76c4e6ce3353756ce8f7ad49b11ffc0c
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-23 18:09:54 +0000

    Bugfixes to checkout.html and addition of noimage file, with associated settings.

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
+                'django.template.context_processors.media',
             ],
             # Ensure tags are available project wide
             'builtins': [
