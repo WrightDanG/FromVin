c33f7021b27d9d6673acab87645f4b7fe5e4c440
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-24 18:00:27 +0000

    Amend stripe key references and add stripe to requirements.txt

--- checkout/views.py
+++ checkout/views.py
@@ -38,8 +38,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
