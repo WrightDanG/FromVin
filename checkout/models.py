afa34b337152e6fc421e1d0365b033516fb1db5c
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-26 18:10:55 +0000

    Add unique order fields to models.py and allow client secret to pass from the view in checkout.html

--- checkout/models.py
+++ checkout/models.py
@@ -25,6 +25,10 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
+    # Unique values to each purchase to check for multiple orders with the same products
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
