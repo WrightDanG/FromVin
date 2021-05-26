63b7b3a3a6b7175b3e879a09bfe74847b84a2042
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-26 18:11:24 +0000

    Add the new unique order fields to admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -15,14 +15,16 @@ class OrderAdmin(admin.ModelAdmin):
     # Ensure that these important fields cannot be edited
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid',)
 
     # Everything required for an order, in this sequence
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid',)
 
     # Only show the following fields in the order list
     list_display = ('order_number', 'date', 'full_name',
