2ab1fa5294e4f048730010b3d033c8cfea87461e
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 19:04:03 +0000

    Add mimetypes as workaround for css loading issue with Heroku

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 import dj_database_url
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
