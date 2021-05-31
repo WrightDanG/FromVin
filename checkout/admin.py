8fb0ad9dd91ff6d9ce02e26c5b1447f216fda9ea
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 15:43:26 +0000

    Checkout success link addition to profile, add profile to admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -19,7 +19,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid',)
 
     # Everything required for an order, in this sequence
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
