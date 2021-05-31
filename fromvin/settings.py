1d35867a6d330fbf512289dfc82fbd528abc62f9
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 19:06:31 +0000

    Additonal mimetypes settings.py amendment.

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -16,6 +16,7 @@ from django.contrib.messages import constants as messages
 import dj_database_url
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
