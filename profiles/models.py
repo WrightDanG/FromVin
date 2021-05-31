e5cb5ecb111f13816b858d47232a1be73d367469
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 14:56:51 +0000

    Add form for profiles app and amend model label for countryfield

--- profiles/models.py
+++ profiles/models.py
@@ -24,7 +24,7 @@ class UserProfile(models.Model):
         max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_country = CountryField(
-        blank_label='Country *', null=True, blank=True)
+        blank_label='Country', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
 
     # Users username
