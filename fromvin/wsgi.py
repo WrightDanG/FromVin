6f4a986911e103230eb5cdf49b7f853b949449b8
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 21:51:00 +0000

    Look to serve media files using wsgi.py amendment to whitenoise

--- fromvin/wsgi.py
+++ fromvin/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+
+from whitenoise import WhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fromvin.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(
+    DjangoWhiteNoise(get_wsgi_application()),
+    root=settings.MEDIA_ROOT,
+    prefix='/media/',
+)
