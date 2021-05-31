6c59cda69580a73f1feff9402337d51baa38cdd4
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 14:58:21 +0000

    Add post information for updating user profile. Add javascript to amend css on profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -7,9 +9,22 @@ def profile(request):
     # Fetch the profile for the user
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        # Populate user's profile
+        form = UserProfileForm(request.POST, instance=profile)
+        # Save
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    # Get all orders attached to the profile
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
