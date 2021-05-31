58e0720515f88f3ea7fd5f5da682d80192075383
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 17:10:29 +0000

    Start to add ability to edit products as a superuser.

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # Needs to be an integer as otherwise clashes with the add url
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>', views.edit_product, name='edit_product'),
 ]
