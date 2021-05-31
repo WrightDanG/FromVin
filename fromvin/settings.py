31df51e9bcadb0fb1029d6d670483c810cd94049
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 18:56:23 +0000

    Add heroku settings for deployment

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(hzm&&&uux41plxp%#5q*)8*9&#!p15k=y+cm6k!m4)9!sj(4t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fromvin.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,16 +124,17 @@ WSGI_APPLICATION = 'fromvin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-# DATABASES = {
-#     'default': dj_database_url.parse()
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
