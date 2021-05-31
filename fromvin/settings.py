eb04ecff8e15fe1d4b7d0646695a2bf934529698
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 18:50:51 +0000

    Begin to deploy to heroku and port over database.

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse()
+# }
 
 
 # Password validation
