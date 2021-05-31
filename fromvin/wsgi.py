6aec4399c34bd881c7b62bf5c02cece95faf773a
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-05-31 21:56:44 +0000

    Remove whitenoise from wsgi.py due to heroku production errors

--- fromvin/wsgi.py
+++ fromvin/wsgi.py
@@ -16,10 +16,4 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fromvin.settings')
 
-# application = get_wsgi_application()
-
-application = WhiteNoise(
-    DjangoWhiteNoise(get_wsgi_application()),
-    root=settings.MEDIA_ROOT,
-    prefix='/media/',
-)
+application = get_wsgi_application()
