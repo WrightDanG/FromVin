f302188e1cd5486f81e79911c5b3ec20098c8f6d
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-06-26 16:59:57 +0000

    Add basic layout to main guide page (index.html)

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -163,7 +163,7 @@ if 'DEVELOPMENT' not in os.environ:
         'CLOUDINARY_URL': os.getenv('CLOUDINARY_URL', '')
     }
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-    NOIMAGE_URL = 'https://res.cloudinary.com/hytpjm4e2/image/upload/v1/media/'
+    NOIMAGE_URL = 'https://res.cloudinary.com/hytpjm4e2/image/upload/v1/media/noimage.jpg'
 else:
     NOIMAGE_URL = '/media/'
 
