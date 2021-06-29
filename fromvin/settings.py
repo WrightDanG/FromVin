bf33dec503de9cad2a364464dc9f7f7231323ee3
WrightDanG <daniel-g-wright@hotmail.co.uk>
2021-06-29 18:51:46 +0000

    Amend settings to test password reset error.

--- fromvin/settings.py
+++ fromvin/settings.py
@@ -181,24 +181,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        )
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.MinimumLengthValidator',
-        )
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.CommonPasswordValidator',
-        )
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.NumericPasswordValidator',
-        )
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+
     },
 ]
 
